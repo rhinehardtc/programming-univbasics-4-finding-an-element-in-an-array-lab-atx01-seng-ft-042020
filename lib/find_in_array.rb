f351a3c35d3ced3fccfb34e597ebfedc9f2216bf
Cameron Rhinehardt <rhinehardtc@gmail.com>
2020-04-02 22:51:33 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,3 +1,9 @@
 def find_element_index(array, value_to_find)
-  array.index(value_to_find)
+  i = 0
+  while i < array.length do
+    if array[i] == value_to_find
+      i
+    end
+    i += 1
+end
 end
